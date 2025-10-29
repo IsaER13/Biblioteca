6c90c5140978b9e5ca357aba9bc2c8ca02d33f1c
IsaER13 <isabel.estrada@portales.edu.co>
2025-10-29 17:45:02 -0500

    primera versión

---
+++ chatbot.py
@@ -0,0 +1,154 @@
+import flet as ft
+import os
+import json
+comandos = {
+    "add":"agregar",
+    "remove" : "eliminar",
+    "update" : "actualizar",
+    "list" : "listar" 
+}
+def load_books():
+    with open("libros.json", "r", encoding="utf-8") as file:
+        books = json.load(file)
+
+    return books 
+books = load_books()
+def save_books(books):
+    with open("libros.json", "w", encoding="utf-8") as files:
+        json.dump(books,files)
+def main (page: ft.Page):
+    page.title = "biblioteca"
+    messages = ft.Column(scroll = True, expand = True, width = page.width)
+    def show_books():
+        list = ft.Column()
+        for book in books:
+            list.controls.append( 
+                ft.ListTile(
+                title = ft.Text( book["Titulo"]), 
+                subtitle = ft.Text (book["Autor"]),
+                on_click =lambda e, l=book: show_details(l,list)
+            ))
+        return list   
+    def update(book):
+        print(book)
+        titulo =  ft.TextField( label = "titulo", value = book ["Titulo"])
+        autor =  ft.TextField(label = "Autor", value = book ["Autor"])
+        portada = ft.TextField(label = "Portada", value = book ["Portada"])
+        comentarios = ft.TextField(label = "comentarios", value = book ["comentarios"])
+        calificacion = ft.TextField(label = "calificacion", value = book ["calificacion"])
+        def on_save(e):
+            data = {
+            "Titulo" : titulo.value,
+            "Autor" : autor.value,
+            "Portada" : portada.value,
+            "comentarios" : comentarios.value,
+            "calificacion" : calificacion.value
+            }
+            books.append(data)
+            save_books(books)
+            messages.controls.append(ft.Text("libro actualizado exitosamente") )
+            page.update() 
+        buttons = ft.Button("Guardar", on_click =  on_save)
+        messages.controls.append(
+            ft.Column(
+                controls = [titulo, autor, portada, comentarios, calificacion, buttons]
+                       
+            )
+        ) 
+        page.update()
+    def confirm(book):
+        confirmation = ft.Column(
+            controls=[
+                ft.Text("¿Estás seguro que quieres eliminar el libro?"),
+                ft.Row(
+                    controls=[
+                        ft.Button("SI", on_click=lambda e, b=book: delete(b)),
+                        ft.Button("NO", on_click=lambda e, b=book: show_books())
+                    ]
+                )
+            ]
+        )
+        messages.controls.append(confirmation)
+        page.update()
+    def delete(book):
+        books.remove(book)
+        save_books(books)
+        messages.controls.append(ft.Text("libro eliminado exitosamente"))
+        page.update()
+    def show_details(book,list):
+        row =  ft.Row(
+                    controls = [
+                    ft.Image(book["Portada"], width = 350, height = 350),
+                    ft.Column(
+                        controls=[
+                    ft.Text(book["Titulo"]),
+                    ft.Text(book["Autor"]),
+                    ft.Text(book["comentarios"]),
+                    ft.Text(book["calificacion"]),
+                    ft.Row(
+                        controls=[
+                            ft.Button("eliminar", on_click=lambda e, b=book: confirm(b)),
+                            ft.Button("actualizar", on_click=lambda e, b=book: update(b))
+                                ]
+                            )
+                        ] 
+                    )  
+                 ]
+             )
+        list.controls.append(row)
+        page.update()
+    def add_book():
+        titulo = ft.TextField(label ="Título")
+        autor = ft.TextField( label= "Autor")
+        comentarios = ft.TextField(label = "comentarios")
+        calificacion = ft.TextField(label ="calificación")
+        portada = ft.TextField(label = "portada")
+        def on_save():
+            data = {
+            "Titulo" : titulo.value,
+            "Autor" : autor.value,
+            "Portada" : portada.value,
+            "comentarios" : comentarios.value,
+            "calificacion" : calificacion.value
+            }
+            books.append(data)
+            save_books(books)
+            messages.controls.append(ft.Text("Libro guardado exitosamente!"))
+            page.update()
+        boton  = ft.Button("Guardar", on_click = lambda e: on_save())
+        return ft.Column ([titulo, autor, portada, comentarios, calificacion, boton])
+    def add_message(e):
+        message = text_area.value.strip()
+        if message == "list":
+            messages.controls.append(show_books())
+        elif message == "add":
+           messages.controls.append(add_book())
+        else:
+            messages.controls.append(ft.Text(message))
+        text_area.value = ""  # limpiar después de enviar
+        messages.scroll_to(offset = -1, duration = 1000)
+        page.update()
+ 
+    text_area=ft.TextField(
+    hint_text="comandos:".join(comandos.keys()),
+    multiline= False,
+    min_lines=1,
+    max_lines=10,
+    expand=False,
+    height=150,
+    on_submit = add_message
+    )
+    page.add(
+        ft.Container(
+        expand = True, 
+        width = page.width,
+        height = page.height,
+        content = messages
+    ), 
+    text_area
+    )
+ft.app(main)
+
+
+
+
